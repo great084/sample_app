891e9acce488dfe54da1f4a52a8b6cf72a93b214
hiromichi <great084@gmail.com>
2018-03-08 13:02:15 +0000

    finish user regestration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "users/show"
-  root 'static_pages#home'
+  #get "users/show"
   
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
-  devise_for :users
+ 
+  root 'static_pages#home'
+
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
