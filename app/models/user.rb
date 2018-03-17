89029f2769d9cd12524f9d42d2ccbd84e1ce33bb
hiromichi <great084@gmail.com>
2018-03-17 12:39:49 +0000

    Make a relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,31 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through:  :relationships, source: :followed 
+  has_many :reverse_relationships, foreign_key: "followed_id", 
+                                   class_name: :Relationship,
+                                   dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :following
   validates :name,  presence: true, length: { maximum: 50 }
   
   def feed
-    #Micropost.where("user_id = ?", id)
-    Micropost
+    Micropost.where("user_id = ?", id)
+    #Micropost
   end
+  
+  def following?(other_user)
+    relationships.find.by(follow_id :other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create.by(follow_id :other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    relationships.find.by(follow_id :other_user.id).destroy
+  end
+
+
+  
 end
